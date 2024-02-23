9adb4486ad295652d580ed71d990a2eed3364673
Yusufhoja <usufhozah@gmail.com>
2024-02-23 17:09:59 +0500

    js 1-dars

---
+++ script.js
@@ -0,0 +1,10 @@
+
+    
+    var  a=prompt("Ismingizni kiriting")
+    var  b=prompt("Yoshingizni kiriting")
+    var  c=prompt("Yoqtirgan faningizni yozing")
+    var  d=prompt("Yashaydigan tumaningizni kiriting")
+    var  e=alert("malumotlaringizni kiritganiz uchun tashakkur")
+    alert(e)
+    console.log (a,b,c,d);
+    c
